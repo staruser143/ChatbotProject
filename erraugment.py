871e8bf6e4538f9bd526d476f11a726909819876
staruser143 <seriesfan.in@gmail.com>
2026-09-17 15:46:16 +0530

    Create erraugment.py

---
+++ erraugment.py
@@ -0,0 +1,5 @@
+ValueError: list.index(x): x not in list
+
+File "/workspaces/ChatbotProject/augment.py", line 54, in <module>
+    "Select Active Chat:", options=chat_options, index=chat_options.index(st.session_state.current_chat)
+                                                       ~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
